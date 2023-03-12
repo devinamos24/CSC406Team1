6607cc6770961933ba6d153313151dc420910f1a
coreym7 <cmorris3@missouriwestern.edu>
2023-03-11 23:50:18 -0600

    Signed-off-by: coreym7 <cmorris3@missouriwestern.edu>

---
+++ src/jvmMain/kotlin/edu/missouriwestern/csc406team1/BankSystem.java
@@ -0,0 +1,17 @@
+
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class BankSystem {
+   public static void main(String[] args) {
+      ArrayList<Customer> customers = new ArrayList<>();
+      ArrayList<Account> accounts = new ArrayList<>();
+   
+      //Load customer and account data from file
+      
+      //Meet requirements for system 
+      
+      //Save customer and account data to file for permanency
+   }
+}
