8b9be1a493663be2395e29b627ae8106fc26edd5
coreym7 <cmorris3@missouriwestern.edu>
2023-03-28 23:51:56 -0500

    Added CheckingAccount.java

---
+++ src/jvmMain/java/edu/missouriwestern/csc406team1/database/CheckingAccount.java
@@ -0,0 +1,30 @@
+package edu.missouriwestern.csc406team1.database;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Date;
+
+public abstract class CheckingAccount extends Account{
+
+    @NotNull
+    private Double transactionFee;//cost of fee per transaction
+    @Nullable
+    private SavingsAccount backupAccount;//pointer to the account designated as the backup account if setup (must be savings account)
+
+    public CheckingAccount(@NotNull Double balance, @NotNull Date dateOpened, @Nullable Double interestRate, @NotNull Double transactionFee, @Nullable SavingsAccount backupAccount) {
+        super(balance, dateOpened, interestRate);
+        this.transactionFee = transactionFee;
+        this.backupAccount = backupAccount;
+    }
+
+    @NotNull
+    public Double getTransactionFee() { return transactionFee; }
+    public void setTransactionFee(@NotNull Double transactionFee) { this.transactionFee = transactionFee; }
+
+    @Nullable
+    public SavingsAccount getBackupAccount() { return backupAccount; }
+    public void setBackupAccount(@Nullable SavingsAccount backupAccount) { this.backupAccount = backupAccount; }
+
+
+}
