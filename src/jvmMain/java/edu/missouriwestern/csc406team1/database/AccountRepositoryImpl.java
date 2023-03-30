a38fec8e6d1691f38751f01cbc316eaa1782032f
devina1 <78060983+devina1@users.noreply.github.com>
2023-03-30 09:57:23 -0500

    - Implement AccountRepository interface - Implement AccountRepository - Update README for csv columns

--- src/jvmMain/java/edu/missouriwestern/csc406team1/database/AccountRepositoryImpl.java
+++ src/jvmMain/java/edu/missouriwestern/csc406team1/database/AccountRepositoryImpl.java
@@ -1,5 +1,45 @@
 package edu.missouriwestern.csc406team1.database;
 
-public class AccountRepositoryImpl {
-    //TODO Create AccountRepositoryImpl
+import edu.missouriwestern.csc406team1.ArrayListFlow;
+import edu.missouriwestern.csc406team1.database.dao.AccountDao;
+import edu.missouriwestern.csc406team1.database.dao.AccountDaoImpl;
+import edu.missouriwestern.csc406team1.database.model.account.Account;
+
+public class AccountRepositoryImpl implements AccountRepository {
+
+    private final AccountDao accountDao;
+
+    public AccountRepositoryImpl() {
+        accountDao = new AccountDaoImpl();
+    }
+
+    @Override
+    public Account getAccount(String accountNumber) {
+        return accountDao.getAccount(accountNumber);
+    }
+
+    @Override
+    public ArrayListFlow<Account> getAccounts() {
+        return accountDao.getAccounts();
+    }
+
+    @Override
+    public void addAccount(Account account) {
+        accountDao.addAccount(account);
+    }
+
+    @Override
+    public void update(Account account) {
+        accountDao.updateAccount(account);
+    }
+
+    @Override
+    public void delete(Account account) {
+        accountDao.deleteAccount(account.getAccountNumber());
+    }
+
+    @Override
+    public boolean save() {
+        return accountDao.save();
+    }
 }
